c2bf723870b35f859f884d424cc7eb781eb27fa1
Woody <wwoody827@gmail.com>
2026-03-28 17:05:43 +0000

    update imu settings

--- bin/imu_receiver.py
+++ bin/imu_receiver.py
@@ -38,7 +38,7 @@ isrun = True
 def parse_opt(known=False):
     parser = argparse.ArgumentParser()
     # parser.add_argument('--debugs', type=bool, default=False, help='if debug info output in terminal ')
-    parser.add_argument('--port', type=str, default='/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0003-if00-port0', help='the models serial port receive data; example: '
+    parser.add_argument('--port', type=str, default='/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0', help='the models serial port receive data; example: '
                                                                                                                                                     '    Windows: COM3'
                                                                                                                                                     '    Linux: /dev/ttyUSB0')
 
@@ -50,7 +50,7 @@ def parse_opt(known=False):
     return receive_params
 
 
-def read_imu_data(port="/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0003-if00-port0", baudrate=921600, timeout=1):
+def read_imu_data(port="/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0", baudrate=921600, timeout=1):
     try:
         serial_ = serial.Serial(port=port, baudrate=baudrate, bytesize=EIGHTBITS, parity=PARITY_NONE,
                                 stopbits=STOPBITS_ONE,
@@ -224,4 +224,4 @@ def read_imu_data(port="/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Br
 
 
     return result
-read_imu_data(port="/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0003-if00-port0", baudrate=921600, timeout=1)
+read_imu_data(port="/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0", baudrate=921600, timeout=1)
