42ef4d608a975fbf2fd288e81d43962afe71267e
Woody <wwoody827@gmail.com>
2026-04-04 11:41:27 -0700

    update interface for network for jetson

--- source/run_interface.cpp
+++ source/run_interface.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char const *argv[]) {
     std::cout << "Usage networkInterface: " << "eth0 of Q1 robot " << std::endl;
-    std::string networkInterface = "wlan0";
+    std::string networkInterface = "wlP1p1s0";
     G1 g1(networkInterface, true);
 
     while (true) sleep(10);
